7687feb33db0f02cc9aae989963139491c76a64a
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-02-09 20:59:52 -0800

    added some question comments for lab 46

---
+++ script.js
@@ -0,0 +1,8 @@
+// Question 1
+// How do I get my javascript to show up on a blank html page?
+
+//Question 2
+//Is javascript the main programming language used for webpages that include sport statistics and lots of numbers?
+
+//Question 3
+//How can I get my javascript code to interact with css and have graphic designs?
