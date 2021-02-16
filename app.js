c9f8f278d18be97b0e5a385227bbd9ec3bfb8f9f
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-02-16 09:04:21 -0800

    this is my code for lab 48

--- app.js
+++ app.js
@@ -3,33 +3,27 @@
 ////let divide= 12/3;
 
 
-
+//lab 47 code
 //console.log('name:',name);
 //console.log('sum of 1+3:', sum);
 //console.log('12/3:', divide);
 
 
+//my work for lab 48
 
 
+//function partOne() {
 
+    //console.log('name:',name);
+    //console.log('sum of 1+3:', sum);
+    //console.log('12/3:', divide); 
+//}
 
-
-//this is my work for lab 48.
-function partOne(name, sum, divide) {
-let name='Sally';
-let sum= 1+3;
-let divide= 12/3;
-console.log('name');
-console.log('sum of 1+3:', sum);
-console.log('12/3:', divide);
+function partTwo(name, add1, add2, numerator, denominator) {
+    console.log('name:',name);
+    console.log('sum of ' + add1 + ' + ' + add2 + ' is ', add1 + add2);
+    console.log(numerator, ' / ' , denominator, ' is ',  numerator/denominator); 
 }
 
-function partTwo(name, add1, add2, divide1, divide2) {
-    let divide1= '12'
-    let divide2= '3'
-    let add1= '1'
-    let add2= '3'
-    console.log('name: ', name);
-   console.log('sum of ', add1, ' + ', add2, ': ', add1+add2);
-   console.log(divide1, ' / ', divide2, ' is: ', divide1/divide2);
-}
+
+partTwo('Sally', 1, 3, 12, 3);
